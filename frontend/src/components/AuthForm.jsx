f28db7b6a12aea6655848e4d3cebde49d8ee5884
MrKolev <100051403+MrKolev@users.noreply.github.com>
2023-07-14 16:22:00 +0300

    add auth form

--- frontend/src/components/AuthForm.jsx
+++ frontend/src/components/AuthForm.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react';
-
 import classes from './AuthForm.module.css';
+import { Link, useSearchParams } from 'react-router-dom';
 
 export function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
 
-  function switchAuthHandler() {
-    setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
+  const [searchParams] = useSearchParams();
+  const isLogin = searchParams.get(`mode`) === 'login';
+  const rePassword = searchParams.get(`mode`) === 'signup';
+
+  function submit() {
+
   }
 
   return (
     <>
-      <form onSubmit={switchAuthHandler} className={classes.form}>
+      <form onSubmit={submit} className={classes.form}>
         <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
         <p>
           <label htmlFor="email">Email</label>
@@ -21,11 +23,15 @@ export function AuthForm() {
           <label htmlFor="image">Password</label>
           <input id="password" type="password" name="password" required />
         </p>
+        {rePassword && <p>
+          <label htmlFor="image">Repeat Password</label>
+          <input id="rePassword" type="password" name="reRassword" required />
+        </p>}
         <div className={classes.actions}>
-          <button type="submit">
+          <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
             {isLogin ? 'Create new user' : 'Login'}
-          </button>
-          <button>Save</button>
+          </Link>
+          <button type="submit">{isLogin ? 'LOGIN' : "SAVE"}</button>
         </div>
       </form>
     </>
